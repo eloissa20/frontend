f81399c6a5fea47c2dfb1758a872a55df8675867
Eloissa <mariaeloissa.andal@tup.edu.ph>
2024-01-14 15:27:32 +0800

    FINAL

---
+++ src/Components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import './DescriptionBox.css'
+
+
+const DescriptionBox = () => {
+  return (
+    <div className='descriptionbox'>
+        <div className="descriptionbox-navigator">
+            <div className="descriptionbox-nav-box">Description</div>
+            <div className="descriptionbox-nav-box fade">Reviews (122)</div>
+        </div>
+        <div className="descriptionbox-description">
+            <p>A lightweight, usually knitted, pullover shirt, close-fitting and wi a 
+                round neckline and short sleeves, worn as an undershirt or outer garment.</p>
+            <p>
+                A lightweight, usually knitted, pullover shirt, close-fitting and wi a 
+                round neckline and short sleeves, worn as an undershirt or outer garment. 
+            </p>
+        </div>
+    </div>
+  )
+}
+
+export default DescriptionBox
